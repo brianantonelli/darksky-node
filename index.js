1c71d74132966c7836319840077ec8dac063de33
Brian <brian.antonelli@gmail.com>
2016-09-25 21:16:11 -0400

    time machine is now working

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 var DarkSky = require('./lib/darksky-api');
 
 var ds = new DarkSky('');
-ds.forecast(33.9, -84.3, {}, function(err, forecast){
-    console.log(err, forecast);
+
+ds.forecast(33.9, -84.3, { exclude: [ 'minutely', 'hourly', 'daily', 'alerts', 'flags' ] }, function(err, forecast){
+    console.log('forecast', forecast);
+})
+
+ds.timemachine(33.9, -84.3, new Date(), {}, function(err, forecast){
+    console.log('timemachine', forecast);
 })
